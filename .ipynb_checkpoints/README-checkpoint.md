99a85e9ef269f2681d9d89b58894255d49a017bf
Haidi Azaman <muhammadhaidi.a.interns@aux.gojek.com>
2024-01-25 07:06:17 +0000

    removed unnecessary files

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,4 +0,0 @@
-# face-recognition-model-development
-Model development part of interview project
-
-download the dataset from https://www.kaggle.com/datasets/atulanandjha/lfwpeople
--- train/.ipynb_checkpoints/runs-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-file_run="/home/jovyan/face-recognition-model-development/train/train.py"
-python "$file_run" --config "/home/jovyan/face-recognition-model-development/train/configs/v1.0.0.yaml"
